09c601690c279d7a4bb36e14d682a283dc17605c
Nick <nzela002@fiu.edu>
2020-02-14 17:32:12 -0500

    geektext

---
+++ geektext/src/components/pages/reviews.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+
+class Reviews extends Component {
+    render(){
+    return (
+        <div className = "container-fluid">
+     <h2>
+         Reviews 
+     </h2>
+     <p>
+         Reviews will be displayed here.
+     </p>
+     </div>
+    );
+  }}
+  
+  export default Reviews;
